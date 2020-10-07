7c5efd82bdc1ceec4e28de6c0f7b4c64c941a35e
Inderpal Singh <singhinther@gmail.com>
2020-10-07 12:30:07 +0200

    configuration specs

--- spec/cirro_io/client/configuration_spec.rb
+++ spec/cirro_io/client/configuration_spec.rb
@@ -1,7 +1,25 @@
 require 'cirro_io/client/configuration'
 
 RSpec.describe CirroIO::Client::Configuration do
-  it 'has a version number' do
-    # expect(CirroIO::Client::VERSION).not_to be nil
+  let(:configuration) { described_class.new }
+
+  it 'assigns app_id' do
+    configuration.app_id '12345'
+
+    expect(configuration.app_id).to eq('12345')
+  end
+
+  it 'raise exception if app_id is not set' do
+    expect { configuration.app_id }.to raise_error('app_id must be defined')
+  end
+
+  it 'assigns private_key_path' do
+    configuration.private_key_path './storage/cirro.pem'
+
+    expect(configuration.private_key_path).to eq('./storage/cirro.pem')
+  end
+
+  it 'raise exception if private_key_path is not set' do
+    expect { configuration.private_key_path }.to raise_error('private_key_path must be defined')
   end
 end
