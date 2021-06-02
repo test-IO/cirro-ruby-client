981e8ecd22a3e92aab82df8471905a42e4b8e781
Jiayi Zheng <thebluber@gmail.com>
2021-06-02 16:00:43 +0200

    Add reward resource & bump version to 1.5.0

--- spec/cirro_io/client/reward_spec.rb
+++ spec/cirro_io/client/reward_spec.rb
@@ -5,11 +5,19 @@ RSpec.describe CirroIO::Client::Reward do
     end
 
     let(:app_worker) { CirroIO::Client::AppWorker.load(id: 1) }
+    let(:attributes) do
+      {
+        title: Faker::Hipster.sentence,
+        description: Faker::Hipster.paragraph,
+        'billing-date': Date.today.to_s,
+        amount: 100,
+      }
+    end
 
     it 'creates a reward for an app worker' do
       request_url = "#{test_site}/v1/rewards"
       stub_request(:post, request_url)
-        # .to_return(body: File.read('./spec/fixtures/app_worker.json'), headers: { 'Content-Type' => 'application/json' })
+        .to_return(body: File.read('./spec/fixtures/reward.json'), headers: { 'Content-Type' => 'application/json' })
 
       request_body = {
         data: {
@@ -18,33 +26,19 @@ RSpec.describe CirroIO::Client::Reward do
             'app-worker': {
               data: {
                 type: 'app-workers',
-                id: 1
-              }
-            }
+                id: 1,
+              },
+            },
           },
-          attributes: {
-            title: 'Title',
-            description: 'Description',
-            'billing-date': Date.today.to_s,
-            amount: 100
-          }
-        }
+          attributes: attributes,
+        },
       }
 
-      described_class.create(title: 'Title', description: 'Description', billing_date: Date.today.to_s, amount: 100, app_worker: app_worker)
+      reward = described_class.create(**attributes.merge(app_worker: app_worker))
 
       expect(a_request(:post, request_url).with(body: request_body.to_json)).to have_been_made
-      # stub_request(:get, "#{test_site}/v1/app-users/4")
-      #   .to_return(body: File.read('./spec/fixtures/app_user.json'), headers: { 'Content-Type' => 'application/json' })
-      #
-      # app_worker = described_class.new
-      # app_worker.app_user = CirroIO::Client::AppUser.find(4).first
-      # app_worker.worker_document = {}
-      #
-      # app_worker.save
-      #
-      # expect(app_worker).to be_valid
-      # expect(app_worker.id).to eq('1')
+      expect(reward).to be_valid
+      expect(reward.id).to eq('1')
     end
   end
 end
