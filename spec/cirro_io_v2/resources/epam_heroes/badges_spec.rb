8c825fdfb7f2933c6a3feb0279b4a5bd13a3716c
Rostik Yuriev <rostyslav_yuriev@epam.com>
2022-12-01 13:38:52 +0200

    Fix lint

--- spec/cirro_io_v2/resources/epam_heroes/badges_spec.rb
+++ spec/cirro_io_v2/resources/epam_heroes/badges_spec.rb
@@ -1,17 +1,15 @@
 RSpec.describe CirroIOV2::Resources::EpamHeroes::Badges do
   let(:site) { 'http://api.cirro.io' }
   let(:client) { CirroIOV2::Client.new(private_key: File.read('./spec/fixtures/private_key.pem'), client_id: 1, site: site) }
+  let(:api_badge_data) { File.read('./spec/fixtures/epam_heroes/badges/create.json') }
 
   describe '#create' do
-    let(:api_badge_data) { File.read('./spec/fixtures/epam_heroes/badges/create.json') }
-    let(:parsed_badge_data) { JSON.parse(api_badge_data).deep_symbolize_keys }
     let(:params) { { to: '1', comment: 'MyComment', type: 'MyEvent', from: '2', emails: ['test@example.com', 'test@test.com'] } }
-    let(:request_heroes_stub) { stub_request(:post, "#{site}/v2/epam_heroes/badges").to_return(body: api_badge_data) }
-
-    before { request_heroes_stub }
+    let!(:request_heroes_stub) { stub_request(:post, "#{site}/v2/epam_heroes/badges").to_return(body: api_badge_data) }
 
     it 'calls creation badge event for cirro client' do
       response_data = described_class.new(client).create(params)
+      parsed_badge_data = JSON.parse(api_badge_data).deep_symbolize_keys
 
       expect(request_heroes_stub).to have_been_made
       expect(response_data.class).to eq(CirroIOV2::Responses::EpamHeroesBadgeResponse)
