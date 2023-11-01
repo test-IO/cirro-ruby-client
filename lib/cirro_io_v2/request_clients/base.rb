9a26df2c41be6250f6a4cef01fbc3c28a8ae8a09
Daniel Vysotskyi <daniel_vysotskyi@epam.com>
2023-11-02 00:33:29 +0200

    remove hack for post requests

--- lib/cirro_io_v2/request_clients/base.rb
+++ lib/cirro_io_v2/request_clients/base.rb
@@ -10,13 +10,7 @@ module CirroIOV2
 
         response
       rescue Faraday::ParsingError => e
-        # Temporarily handle "You are being redirected step and make request to target location" 
-        # Strictly unrecommended to show this hack to children and to people with heart disease
-        raise Errors::ResponseNotJsonError, e unless e.response.status >= 300
-
-        # args [method, path]
-        args[1] = e.response.headers['location']
-        request(*args, **named_args)
+        raise Errors::ResponseNotJsonError, e
       end
 
       def make_request(*args)
